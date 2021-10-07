f324308e24bfd6351e8dde94d04f7192ecdfc0b3
nhrnhr0 <nhrnhr0@gmail.com>
2021-10-07 19:41:26 +0300

    first commit

---
+++ core/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.contrib.admin.sites import site
+from .models import ApprovedOrders, GeneralOrders
+# Register your models here.
+class GeneralOrdersAdmin(admin.ModelAdmin):
+    model = GeneralOrders
+    list_display = ('created', 'name', 'providerName', 'total','type',  'isApprove')
+    list_editable = ('name', 'providerName', 'total', 'type', 'isApprove')
+admin.site.register(GeneralOrders, GeneralOrdersAdmin);
+
+
+class ApprovedOrdersAdmin(admin.ModelAdmin):
+    model = ApprovedOrders
+    fieldsets = (
+        (None, {
+            "fields": (
+                ('parent','invoiceNumber')
+                
+            ),
+        }),
+    )    
+    list_display = ('id', 'parent', 'invoiceNumber')
+    #readonly_fields= ('parent__name',)
+    #list_editable  = ('parent_name',)
+
+    #def parent_name(self, obj):
+        #return obj.parent.name
+admin.site.register(ApprovedOrders, ApprovedOrdersAdmin);
---
+++ core/models.py
@@ -0,0 +1,84 @@
+from django.contrib.admin.options import ModelAdmin
+from django.db import models
+from django.db.models.fields import DateField
+from django.utils import timezone
+
+# Create your models here.
+
+
+class GeneralOrders(models.Model):
+    EMPTY = 'EM'
+    CONSTANT = 'CO'
+    ACQUISITION = 'AC'
+    PROVIDERS = 'PR'
+    TYPE_CHOICES = [
+        (EMPTY, 'בחר אפשררות'),
+        (CONSTANT, 'קבועות'),
+        (ACQUISITION, 'רכש'),
+        (PROVIDERS, 'ספקים'),
+    ]
+    
+    YES = 'YE'
+    NO = 'NO'
+    IS_APPROVE_CHOICES = [
+        (YES, 'כן'),
+        (NO, 'לא'),
+    ]
+    created     = models.DateTimeField(editable=False)
+    modified    = models.DateTimeField(auto_now=True)
+    name        = models.CharField(max_length=250)
+    providerName= models.CharField(max_length=100)
+    total       = models.FloatField(null=True, blank=True)
+    type        = models.CharField(max_length=2,choices=TYPE_CHOICES,default=EMPTY,)
+    isApprove   = models.CharField(max_length=2,choices=IS_APPROVE_CHOICES,default=NO,)
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+
+
+        if self.isApprove == GeneralOrders.YES:
+            print('is approve is true')
+            data, created = ApprovedOrders.objects.get_or_create(parent=self,)
+
+
+        return super(GeneralOrders, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name + '(' + str(self.total) + '₪)'
+
+
+class ApprovedOrders(models.Model):
+    NOT_PAID = 'NP'
+    BACK_TRANSFER = 'BT'
+    DIRECT = 'DR'
+    CREDIT_CARD = 'CC'
+    CHECK = 'CH'
+    CACHE = 'CA'
+    PAID_CHOICES = [
+        (NOT_PAID, 'לא שולם'),
+        (BACK_TRANSFER, 'העברה בנקאית - ירוק'),
+        (DIRECT, 'דיירקט עידן - צהוב'),
+        (CREDIT_CARD, 'אשראי אופיר - כתום'),
+        (CHECK, 'צ\'ק נייר - אדום'),
+        (CACHE,'מזומן - כחול'),
+    ]
+    IN_PC = 'PC'
+    PHYSICAL = 'PH'
+    DELIVERY = 'DL'
+    EMPTY = 'EM'
+    
+    INVOICE_CHOICES = [
+        
+        (IN_PC, 'מסמך בתקייה במחשב - ירוק'),
+        (PHYSICAL, 'קובץ נייר במגירה - צהוב'),
+        (DELIVERY, 'במשלוח פיזי למשרד - כתום'),
+        (EMPTY, 'אין חשבונית - אדום'),
+    ]
+
+    parent = models.ForeignKey(to=GeneralOrders, on_delete=models.CASCADE) #models.OneToOneField(to=GeneralOrders, on_delete=models.CASCADE)
+    invoiceNumber = models.CharField(max_length=50, blank=True)
+    paidHow = models.CharField(max_length=2, choices=PAID_CHOICES,default=NOT_PAID)
+    whenToPay = models.DateField(blank=True, null=True)
+    invoiceLocation = models.CharField(max_length=2, choices=INVOICE_CHOICES, default=EMPTY)
