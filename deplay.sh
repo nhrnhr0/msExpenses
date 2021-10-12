9fc9500aa624b27f64a7614fe1281652d3e7da41
Ubuntu <ubuntu@ip-172-31-17-51.us-east-2.compute.internal>
2021-10-12 14:48:18 +0000

    updates from the server

--- deplay.sh
+++ deplay.sh
@@ -1,5 +1,6 @@
-source env/scripts/activate
-pip install -r requirements.txt
-python manage.py migrate
+source ./env/bin/activate
+pip install -r requirments.txt
+python3 manage.py migrate
+python3 manage.py collectstatic --no-input
 sudo service nginx restart
 sudo supervisorctl restart gunicornMsExpenses
